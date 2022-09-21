537afe7f6ffb33a5712f406ba20e1ff036562a5a
Sarwar Hossain <112124883+codersarwar@users.noreply.github.com>
2022-09-21 15:45:38 +0530

    Add files via upload

---
+++ asign7.py
@@ -0,0 +1,3 @@
+for i in range(21,1,-1):
+    if i%2==0:
+        print("even natarual number are in reverse : ",i)
---
+++ assign9.py
@@ -0,0 +1,5 @@
+for i in range(1,21,1):
+    if i%2==0:
+        print("qube of firt 10 natarual number : ",i**3)
+        
+    
